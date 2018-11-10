ba537725cd3311dfccfb2fb3dcfd641f9484f809
= <bpalonis59@gmail.com>
2018-11-10 16:13:04 -0500

    Finished the program

--- truth.rb
+++ truth.rb
@@ -1,23 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-html = ''
-
 get '/' do
   error_msg = ''
   erb :index, locals: { error_msg: error_msg }
 end
 
-post '/table/:true/:false/:size' do
-  # If the button is clicked with no input, input is set
-  if table_size_str.empty? && true_symbol.empty? && false_symbol.empty?
-    true_symbol = 'T'
-    false_symbol = 'F'
-    table_size = 3
-    erb :table, locals: { true_symbol: true_symbol, false_symbol: false_symbol, table_size: table_size, html: html }
-  end
+get '/table' do
+  true_symbol = params[:true_val]
+  false_symbol = params[:false_val]
+  table_size_str = params[:size]
+  true_symbol = 'T' if true_symbol.empty?
+  false_symbol = 'F' if false_symbol.empty?
+  table_size_str = '3' if table_size_str.empty?
   table_size = table_size_str.to_i
-
   # Error Messages if:
   # => the true or false symbol are more than a symbol
   # => true == false
@@ -34,7 +30,17 @@ post '/table/:true/:false/:size' do
     error_msg = 'Size must be >= 2'
     erb :index, locals: { error_msg: error_msg }
   end
-  erb :table, locals: { true_symbol: true_symbol, false_symbol: false_symbol, table_size: table_size, html: html }
+
+  num_rows = 2**table_size
+  num_cols = table_size + 3
+  table = create_table num_rows, num_cols, true_symbol, false_symbol, table_size
+
+  erb :table, locals: {
+    table: table,
+    num_rows: num_rows,
+    num_cols: num_cols,
+    table_size: table_size
+  }
 end
 
 not_found do
@@ -42,48 +48,47 @@ not_found do
   erb :error, locals: { status: status }
 end
 
-get '/table' do
-  true_symbol = params['true'].to_s
-  false_symbol = params['false'].to_s
-  table_size_str	= params['size'].to_s
-  # 2^x for number of combinations and then add 1 for the header row
-  num_rows = (2**table_size) + 1
-  # Add 3 for the And, Or, Xor columns
-  num_cols = table_size + 3
-  true_false_int_array = Array.new(num_cols) { 0 }
-  saved_combinations = Array.new(num_rows)
-  html = '<table \"width:100%\">'
-
+def create_table(num_rows, num_cols, true_symbol, false_symbol, table_size)
   table = Array.new(num_rows) { Array.new(num_cols) }
-
-  # Adds column headers starting with table_size-1..0
-  html += '<tr>'
-  for i in (table_size-1..0)
-    html += '<th>'
-    html += i.to_s
-    html += '</th>'
+  (0...2**table_size).each do |i|
+    set_row_value i, table, table_size
   end
-  # Add final headrs for And, Or, and XOR
-  html += '<th>AND</th>'
-  html += '<th>OR</th>'
-  html += '<th>XOR</th>'
-  html += '</tr>'
-
-  for i in (0..num_rows-1)
-  html += '<tr>'
-    for j in (0..num_cols-1)
-      html += '<td>'
-      html += true_symbol
-      html += '</td>'
+  table.each.with_index do |row, row_index|
+    row.each.with_index do |col, col_index|
+      table[row_index][col_index] = (col == '1' ? true_symbol : false_symbol)
     end
-    html += '</tr>'
   end
-
-  html += '</table>'
-  erb :table, locals: { html: html }
+  table
 end
 
-def find_string_and_fill_array array
-  k = 0
-  k
+def set_row_value(row, table, table_size)
+  num = (row).to_s(2)
+  num = pad_zeroes(num, table_size)
+  table_size.times do |i|
+    table[row][i] = num[i]
+  end
+  table[row][table_size] = '1'
+  table[row][table_size] = '0' if num.include? '0'
+  table[row][table_size + 1] = '0'
+  table[row][table_size + 1] = '1' if num.include? '1'
+  table[row][table_size + 2] = '0'
+  table[row][table_size + 2] = '1' if check_num_ones? num
+end
+
+def check_num_ones?(num)
+  num_ones = 0
+  num.chars.each do |letter|
+    num_ones += 1 unless letter != '1'
+  end
+  num_ones.odd?
+end
+
+def pad_zeroes(str, table_size)
+  i = str.length
+  str_two = ''
+  while i < table_size do
+    str_two += '0'
+    i += 1
+  end
+  str_two + str
 end
